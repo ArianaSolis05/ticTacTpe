7da9e1f686d08450272a25bdada731786c5c2c00
Singular3 <jcr8026@gmail.com>
2025-08-21 15:53:18 -0600

    Commit

--- src/js/proyecto.js
+++ src/js/proyecto.js
@@ -7,51 +7,95 @@ const casilla5 = document.getElementById("casilla5")
 const casilla6 = document.getElementById("casilla6")
 const casilla7 = document.getElementById("casilla7")
 const casilla8 = document.getElementById("casilla8")
+const reiniciar = document.getElementById("reiniciar")
 
 
-const arreglo = [casilla0,casilla1,casilla2,casilla3,casilla4,casilla5,casilla6,casilla7,casilla8]
+
+let movimientosTablero = 0
+let contadorX = localStorage.getItem("contadorX") || 0;
+let contadorO = localStorage.getItem("contadorO") || 0;
+
+const victoriasO = document.getElementById("victoriasO")
+victoriasO.textContent = contadorO
+const victoriasX = document.getElementById("victoriasX")
+victoriasX.textContent = contadorX
+
+
+const arreglo = [casilla0, casilla1, casilla2, casilla3, casilla4, casilla5, casilla6, casilla7, casilla8]
 
 
 function movimientos() {
     arreglo.forEach((casilla) => {
-        casilla.addEventListener("click",()=>{
+        casilla.addEventListener("click", () => {
             if (casilla.textContent == "") {
+                //  const img = document.createElement("img")
+                //  img.setAttribute("src","../imagenes/carro.webp")
+                //  img.classList.add("imgX")
+                // casilla.appendChild(img)
+
                 casilla.textContent = "X"
-                setTimeout(() => {
-                    aleatorio()
-                }, 300)
+                movimientosTablero++
+                validarGanador
+                if (!validarGanador()) {
+                    setTimeout(() => {
+                        aleatorio()
+                    }, 500)
+                }
             }
         })
     })
 }
 
 function aleatorio() {
-    const posicionesVacias= arreglo.filter((casilla)=> casilla.textContent == "");
+    movimientosTablero++
+    const posicionesVacias = arreglo.filter((casilla) => casilla.textContent == "");
     if (posicionesVacias.length > 0) {
         const posAleatoria = [Math.floor(Math.random() * posicionesVacias.length)]
         posicionesVacias[posAleatoria].textContent = "O"
-        
+        validarGanador()
     }
 }
 movimientos()
 
-/*
-    DENTRO DE UNA FUNCIÓN
-    Vamos a crear un arreglo de arreglos con las posibles pos ganadoras
-    Dentro ed la función crear un arreglo que recorra ese arreglo de arreglos
 
-*/
-function validarGanador(){
+let alguieGana = false
+function validarGanador() {
+    console.log(movimientosTablero);
     const posiblesGanes = [
-        [0,1,2],[3,4,5],[6,7,8],// -> Estás son las filas
-        [0,3,6],[1,4,7],[2,5,8], // -> Estás son las columnas 
-        [0,4,8],[2,4,6], // -> Estás son las diagonales
-    ] 
-    for (let index = 0; posiblesGanes < array.length; index++) {
-        const [posicion,posicion2,posicion3] = posiblesGanes [index];
-       if (posiblesGanes [posicion].textContent !=="" && posiblesGanes[posicion]=== posiblesGanes[posicion2].textContent && posiblesGanes[posicion].textContent===posiblesGanes [posicion3]) {
-        
-       }
-        
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],// -> Estás son las filas
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // -> Estás son las columnas 
+        [0, 4, 8], [2, 4, 6] // -> Estás son las diagonales
+    ]
+    for (let index = 0; index < posiblesGanes.length; index++) {
+        const [posicion, posicion2, posicion3] = posiblesGanes[index]
+        if (arreglo[posicion].textContent != "" && arreglo[posicion].textContent == arreglo[posicion2].textContent && arreglo[posicion].textContent == arreglo[posicion3].textContent) {
+            alert("Hay un ganador")
+            if (arreglo[posicion].textContent === "X") {
+                contadorX++;
+                victoriasX.textContent = contadorX
+                localStorage.setItem("contadorX", contadorX)
+                alguieGana = true
+            } if (arreglo[posicion].textContent === "O") {
+                contadorO++;
+                victoriasO.textContent = contadorO
+                localStorage.setItem("contadorO", contadorO)
+                alguieGana = true
+            }
+            if (movimientosTablero >= 9 && !alguieGana) {
+                alert("EMPATE")
+                return true
+            }
+
+            return true
+        }
     }
+
 }
+
+function reiniciarF() {
+    arreglo.forEach(casilla => casilla.textContent = "")
+
+
+}
+
+document.getElementById("reiniciar").addEventListener("click", reiniciarF)
